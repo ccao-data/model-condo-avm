0cb6a13781355ed2f9612c24f3899198ea94b60e
Dan Snow <DSnow@cookcountyassessor.com>
2020-10-27 16:43:39 -0500

    Initial commit

---
+++ R/recipes.R
@@ -0,0 +1,59 @@
+# Main data prep recipe, removes unnecessary variables, cleans up factors,
+# removes low/no variance and highly correlated vars, log + polynomial trans
+mod_recp_prep <- function(data, keep_vars, id_vars) {
+  recipe(meta_sale_price ~ ., data = data) %>%
+    
+    # Convert PIN to numeric so it's not set to NA by missing factor levels
+    # Set PIN role to "id" so it's not included in the actual fit
+    update_role(any_of(id_vars), new_role = "id") %>%
+    step_mutate_at(has_role("id"), fn = ~ as.numeric(as.character(.))) %>%
+    step_rm(-any_of(keep_vars), -all_outcomes(), -has_role("id")) %>%
+    
+    # Preprocessing for all predictors and outcome
+    step_unknown(all_nominal(), -has_role("id")) %>%
+    step_other(
+      all_nominal(), -has_role("id"), -any_of(c("meta_town_code", "meta_nbhd")),
+      threshold = 0.005
+    ) %>%
+    step_naomit(
+      all_predictors(), all_outcomes()
+    ) %>%
+    step_log(
+      all_outcomes(), ends_with("_sf"), contains("income"), contains("meta_nbhd_"),
+      offset = 1
+    ) %>%
+    step_nzv(
+      all_predictors(), -all_outcomes(), -has_role("id"),
+      unique_cut = 0.05
+    ) %>%
+    step_corr(
+      all_numeric(), -all_outcomes(), -has_role("id"), 
+      threshold = 0.95
+    ) %>%
+    step_poly(
+      ends_with("_age"), ends_with("_sf"),
+      degree = 2
+    )
+}
+
+
+# Add dummy vars specifically for xgb and enet, not necessary for others
+# (Converts categorical vars to one-hot encoding)
+dummy_recp_prep <- function(recipe) {
+  recipe %>%
+    step_mutate_at(starts_with("ind_"), fn = ~ as.numeric(.)) %>%
+    step_dummy(all_nominal(), -all_outcomes(), -has_role("id"))
+}
+
+
+# Recipe for stacking/meta model, goal here is to interact each vector of fitted
+# model values with spatial regimes variable
+stack_recp_prep <- function(data, group_vars = NULL) {
+  recipe(meta_sale_price ~ ., data = data) %>%
+    add_role(all_numeric(), -all_outcomes(), new_role = "model") %>%
+    step_rm(-any_of(group_vars), -all_outcomes()) %>%
+    step_naomit(all_predictors()) %>%
+    step_log(all_numeric(), all_outcomes()) %>%
+    step_dummy(all_nominal(), -all_outcomes()) %>%
+    step_interact(~ has_role("model") * matches(".X\\d+"))
+}
---
+++ R/valuation_funs.R
@@ -0,0 +1,94 @@
+# Shift the distribution of ratios toward the true median to account for bias
+# in modeling. Not ideal but all we can do with bad data
+val_med_pct_adj <- function(truth, estimate, min_n = 20, max_abs_adj = 0.4) {
+  num_sales <- sum(!is.na(truth))
+  
+  med_pct_adj <- median((truth - estimate) / estimate, na.rm = TRUE)
+  
+  output <- ifelse(num_sales >= min_n, med_pct_adj, NA_real_)
+  output <- ifelse(
+    abs(med_pct_adj) > max_abs_adj,
+    0.4 * sign(med_pct_adj),
+    med_pct_adj
+  )
+  
+  return(output)
+}
+
+
+# Limit excessively high and low ratios by capping them to a fixed boundary
+val_limit_ratios <- function(truth, estimate, lower = 0.7, upper = 2.0) {
+  
+  ratio <- estimate / truth
+  
+  ratio_high <- !is.na(ratio) & ratio > upper
+  ratio_low <-  !is.na(ratio) & ratio < lower
+  
+  estimate[ratio_high] <- truth[ratio_high] * upper
+  estimate[ratio_low]  <- truth[ratio_low] * lower
+  
+  return(estimate)
+}
+
+
+# Within the same developments, townhomes with the same age and building size
+# should have the same value.
+val_townhomes_by_group <- function(data, class, estimate, townhome_adj_cols) {
+  
+  data %>%
+    filter({{class}} %in% c("210", "295")) %>%
+    group_by(across(all_of(townhome_adj_cols))) %>%
+    summarize(
+      med_townhome_adj = median({{estimate}}, na.rm = TRUE),
+      num_in_group = n()
+    ) %>%
+    filter(num_in_group > 1)
+}
+
+
+# Post-valuation adjustment object that saves adjustments and can be called to
+# predict new values
+postval_model <- function(data, truth, class, estimate, med_adj_cols, townhome_adj_cols) {
+  
+  med_adjustments <- data %>%
+    group_by(across(all_of(med_adj_cols))) %>%
+    summarize(
+      med_pct_adj = val_med_pct_adj({{truth}}, {{estimate}}),
+      num_sales = sum(!is.na({{truth}}))
+    ) %>%
+    ungroup()
+  
+  townhome_adjustments <- data %>%
+    val_townhomes_by_group(
+      class = meta_class,
+      estimate = stack,
+      townhome_adj_cols = townhome_adj_cols
+    ) %>%
+    ungroup()
+  
+  output <- list(
+    med_adjustments = med_adjustments,
+    townhome_adjustments = townhome_adjustments
+  )
+  class(output) <- "postval_model"
+  
+  return(output)
+}
+
+
+# Predict method for S3 postval_model object
+predict.postval_model <- function(object, new_data, truth, estimate) {
+  
+  new_data %>%
+    left_join(object$townhome_adjustments) %>%
+    mutate(
+      {{estimate}} := ifelse(!is.na(med_townhome_adj), med_townhome_adj, {{estimate}})
+    ) %>%
+    left_join(object$med_adjustments) %>%
+    mutate(
+      {{estimate}} := rowSums(tibble({{estimate}}, {{estimate}} * med_pct_adj), na.rm = T),
+      {{estimate}} := na_if({{estimate}}, 0),
+      {{estimate}} := val_limit_ratios({{truth}}, {{estimate}})
+    ) %>%
+    pull({{estimate}})
+}
