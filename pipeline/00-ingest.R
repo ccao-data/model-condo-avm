3b9951818db27c76cce32683fbd358eaba733afb
sweatyhandshake <wridgeway@cookcountyassessor.com>
2022-03-03 15:21:59 -0600

    Update with new rexs code

--- pipeline/00-ingest.R
+++ pipeline/00-ingest.R
@@ -4,17 +4,22 @@
 
 # Pre-allocate memory for java JDBC driver
 options(java.parameters = "-Xmx10g")
+Sys.setenv(R_INSTALL_STAGED = FALSE)
 
 # Load R libraries
 library(arrow)
 library(ccao)
 library(DBI)
+library(data.table)
 library(dplyr)
 library(glue)
 library(here)
 library(igraph)
 library(lubridate)
+library(purrr)
 library(RJDBC)
+library(s2)
+library(sf)
 library(tictoc)
 library(tidyr)
 library(yaml)
@@ -107,8 +112,10 @@ rm(AWS_ATHENA_CONN_JDBC, aws_athena_jdbc_driver)
 
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# 3. Clean Data ----------------------------------------------------------------
+# 3. Define Functions ----------------------------------------------------------
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Ingest-specific helper functions for data cleaning, spatial lags, etc.
 
 # Create a dictionary of column types, as specified in ccao::vars_dict
 col_type_dict <- ccao::vars_dict %>%
@@ -131,7 +138,28 @@ recode_column_type <- function(col, col_name, dict = col_type_dict) {
 }
 
 
-## 3.1. Training Data ----------------------------------------------------------
+# Find the K-nearest neighbors within a group to calculate a spatial lag
+st_knn <- function(x, y = NULL, k = 1) {
+  s2x <- sf::st_as_s2(x)
+  if (is.null(y)) {
+    z <- s2::s2_closest_edges(s2x, s2x, k = (k + 1))
+    # Drop the starting observation/point
+    z <- lapply(z, sort)
+    z <- lapply(seq_along(z), function(i) setdiff(z[[i]], i))
+    return(z)
+  } else {
+    s2y <- sf::st_as_s2(y)
+    z <- s2::s2_closest_edges(s2x, s2y, k = k)
+  }
+}
+
+
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# 4. Add Features and Clean ----------------------------------------------------
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+## 4.1. Training Data ----------------------------------------------------------
 
 # Clean up the training data. Goal is to get it into a publishable format.
 # Final featurization, filling, etc. is handled via Tidymodels recipes
@@ -157,16 +185,45 @@ training_data_clean <- training_data %>%
     # Get components of dates for fixed effects to correct seasonality
     time_sale_quarter_of_year = quarter(meta_sale_date),
     time_sale_week_of_year = week(meta_sale_date),
+    
+    # Time window to use for cross-validation and calculating spatial lags
+    time_split = time_interval %/% months(params$input$time_split)
+  )
 
-    # Create indicators for dates that fall in particular months
-    time_sale_during_school_year = month(meta_sale_date) %in% c(1:5, 9:12),
-    time_sale_during_holidays = month(meta_sale_date) %in% c(11, 12, 1)
+# Calculate KNN spatial lags for each N month period used in CV. The N month
+# partitioning ensures that no training data leaks into the validation set
+# during CV
+training_data_lagged <- training_data_clean %>%
+  # Convert coords to geometry used to calculate weights
+  filter(!is.na(loc_longitude), !is.na(loc_latitude)) %>%
+  st_as_sf(
+    coords = c("loc_longitude", "loc_latitude"),
+    crs = 4326,
+    remove = FALSE
   ) %>%
-  select(-any_of("time_interval")) %>%
+  # Divide training data into N-month periods and calculate spatial lags
+  # for each period
+  group_by(time_split) %>%
+  mutate(
+    nb = st_knn(geometry, k = params$input$spatial_lag$k),
+    across(
+      all_of(params$input$spatial_lag$predictor),
+      function(x) purrr::map_dbl(nb, function(idx, var = x) mean(var[idx])),
+      .names = "lag_{.col}"
+    )
+  ) %>%
+  # Clean up output, bind rows that were missing lat/lon, and write to file
+  ungroup() %>%
+  st_drop_geometry() %>%
+  bind_rows(
+    training_data_clean %>%
+      filter(is.na(loc_x_3435) | is.na(loc_y_3435))
+  ) %>%
+  select(-c(nb, time_interval)) %>%
   write_parquet(paths$input$training$local)
 
 
-## 3.2. Assessment Data --------------------------------------------------------
+## 4.2. Assessment Data --------------------------------------------------------
 
 # Clean the assessment data. This the target data that the trained model is 
 # used on. The cleaning steps are the same as above, with the exception of the
@@ -174,7 +231,6 @@ training_data_clean <- training_data %>%
 assessment_data_clean <- assessment_data %>%
   ccao::vars_recode(cols = starts_with("char_"), type = "code") %>%
   mutate(across(everything(), ~ recode_column_type(.x, cur_column()))) %>%
-  
   # Create sale date features BASED ON THE ASSESSMENT DATE. The model predicts
   # the sale price of properties on the date of assessment. Not the date of an
   # actual sale
@@ -182,17 +238,66 @@ assessment_data_clean <- assessment_data %>%
     meta_sale_date = as_date(params$assessment$date),
     time_interval = interval(ymd("1997-01-01"), ymd(.data$meta_sale_date)),
     time_sale_year = year(meta_sale_date),
-    time_sale_week = time_interval %/% weeks(1),
-    time_sale_quarter_of_year = quarter(meta_sale_date),
-    time_sale_week_of_year = week(meta_sale_date),
-    time_sale_during_school_year = month(meta_sale_date) %in% c(1:5, 9:12),
-    time_sale_during_holidays = month(meta_sale_date) %in% c(11, 12, 1)
+    time_sale_day = time_interval %/% days(1),
+    time_sale_quarter_of_year = paste0("Q", quarter(meta_sale_date)),
+    time_sale_day_of_year = day(meta_sale_date),
+    time_split = time_interval %/% months(params$input$time_split)
+  )
+
+# Grab the most recent sale within the last 3 split periods for each PIN. This
+# will be the search space for the spatial lag for assessment data
+sales_data <- training_data_clean %>%
+  filter(
+    meta_sale_date >= as_date(params$assessment$date) -
+      months(params$input$time_split * 3),
+    !ind_pin_is_multicard,
+    !is.na(loc_longitude)
   ) %>%
-  select(-any_of("time_interval")) %>%
+  group_by(meta_pin) %>%
+  filter(max(meta_sale_date) == meta_sale_date) %>%
+  ungroup() %>%
+  st_as_sf(
+    coords = c("loc_longitude", "loc_latitude"),
+    crs = 4326,
+    remove = TRUE
+  ) %>%
+  select(
+    meta_pin, meta_year, meta_sale_price, 
+    all_of(params$input$spatial_lag$predictor),
+  )
+
+# Join the sales data to the assessment data. Create lagged spatial predictors
+# using the K-nearest neighboring sales. This replicates a spatial Durbin model
+# with a time component
+assessment_data_lagged <- assessment_data_clean %>%
+  filter(!is.na(loc_longitude), !is.na(loc_latitude)) %>%
+  st_as_sf(
+    coords = c("loc_longitude", "loc_latitude"),
+    crs = 4326,
+    remove = FALSE
+  ) %>%
+  mutate(
+    nb = st_knn(geometry, sales_data$geometry, k = params$input$spatial_lag$k),
+    meta_sale_price = NA_real_,
+    across(
+      all_of(params$input$spatial_lag$predictor),
+      function(x) purrr::map_dbl(nb, function(idx, var = dplyr::cur_column()) {
+        mean(sales_data[[var]][idx])
+      }),
+      .names = "lag_{.col}"
+    )
+  ) %>%
+  # Clean up output and write to file
+  ungroup() %>%
+  st_drop_geometry() %>%
+  bind_rows(
+    assessment_data_clean %>%
+      filter(is.na(loc_x_3435) | is.na(loc_y_3435))
+  ) %>%
+  select(-c(nb, meta_sale_price, time_interval)) %>%
   write_parquet(paths$input$assessment$local)
 
-
-## 3.4. Land Rates -------------------------------------------------------------
+## 3.3. Land Rates -------------------------------------------------------------
 
 # Write land data directly to file, since it's already mostly clean
 land_nbhd_rate_data %>%
--- pipeline/01-train.R
+++ pipeline/01-train.R
@@ -16,7 +16,7 @@ library(dplyr)
 library(here)
 library(lightgbm)
 library(lightsnip)
-library(purrr)
+library(lubridate)
 library(stringr)
 library(tictoc)
 library(tidymodels)
@@ -60,24 +60,24 @@ set.seed(params$model$seed)
 # there is multiple buildings on a PIN. Sales for multicard PINs are
 # often for multiple buildings and will therefore bias the model training
 training_data_full <- read_parquet(paths$input$training$local) %>%
-  filter(!is.na(loc_longitude), !is.na(loc_latitude)) %>%
   arrange(meta_sale_date)
 
 # Create train/test split by time, with most recent observations in the test set
 # We want our best model(s) to be predictive of the future, since properties are
 # assessed on the basis of past sales
-time_split <- initial_time_split(
+split_data <- initial_time_split(
   data = training_data_full,
   prop = params$cv$split_prop
 )
-test <- testing(time_split)
-train <- training(time_split)
+test <- testing(split_data)
+train <- training(split_data)
 
 # Create a recipe for the training data which removes non-predictor columns and
 # preps categorical data, see R/recipes.R for details
 train_recipe <- model_main_recipe(
-  data = train,
-  keep_vars = params$model$predictor$all,
+  data = training_data_full %>% select(-time_split),
+  pred_vars = params$model$predictor$all,
+  cat_vars = params$model$predictor$categorical,
   id_vars = params$model$predictor$id
 )
 
@@ -108,31 +108,31 @@ lgbm_model <- parsnip::boost_tree(
     engine = params$model$engine,
     seed = params$model$seed,
     
-
+    
     ### 3.1.1. Manual Parameters -----------------------------------------------
-
+    
     # These are static lightgbm-specific engine parameters passed to lgb.train()
     # See lightsnip::train_lightgbm for details
     num_threads = num_threads,
     verbose = params$model$verbose,
-
+    
     # Set the objective function. This is what lightgbm will try to minimize
     objective = params$model$objective,
-
+    
     # Typically set manually along with the number of iterations (trees)
     learning_rate = params$model$parameter$learning_rate,
-
+    
     # Names of integer-encoded categorical columns. This is CRITICAL else
     # lightgbm will treat these columns as numeric
     categorical_feature = params$model$predictor$categorical,
-
+    
     # Enable early stopping using a proportion of each training sample as a
     # validation set. If lgb.train goes stop_iter() rounds without improvement
     # in the chosen metric, then it will end training early. This saves an
     # immense amount of time during CV
     validation = params$model$parameter$validation_prop,
     metric = params$model$parameter$validation_metric,
-
+    
     # Lightsnip custom parameter. Links the value of max_depth to num_leaves
     # using floor(log2(num_leaves)) + add_to_linked_depth. Useful since
     # otherwise Bayesian opt spends time exploring irrelevant parameter space
@@ -140,7 +140,7 @@ lgbm_model <- parsnip::boost_tree(
 
 
     ### 3.1.2. Tuned Parameters ------------------------------------------------
-
+    
     # These are parameters that are tuned using cross-validation. These are the
     # main parameters determining model complexity
     num_leaves = tune(),
@@ -148,13 +148,13 @@ lgbm_model <- parsnip::boost_tree(
     feature_fraction = tune(),
     min_gain_to_split = tune(),
     min_data_in_leaf = tune(),
-
+    
     # Categorical-specific parameters
     max_cat_threshold = tune(),
     min_data_per_group = tune(),
     cat_smooth = tune(),
     cat_l2 = tune(),
-
+    
     # Regularization parameters
     lambda_l1 = tune(),
     lambda_l2 = tune(),
@@ -180,9 +180,23 @@ lgbm_wflow <- workflow() %>%
 # produce similarly accurate results
 if (cv_enable) {
   
-  # Create v-fold CV splits of the main training set
-  train_folds <- vfold_cv(data = train, v = params$cv$num_folds)
-
+  # Using rolling forecast origin resampling to create a cumulative, sliding
+  # window-based training set, where the validation set is always just after the
+  # training set in time. See https://www.tmwr.org/resampling.html#rolling
+  train_folds <- rolling_origin(
+    data = nest(train, data = -time_split),
+    initial = 1, assess = 1, skip = 0, cumulative = TRUE
+  ) %>%
+    mutate(splits = map(splits, ~ make_splits(
+      bind_rows(analysis(.x)$data),
+      bind_rows(assessment(.x)$data)
+    ))) %>%
+    rsample::new_rset(
+      splits = .$splits,
+      ids = .$id,
+      subclass = c("rolling_origin", "rset")
+    )
+  
   # Create the parameter search space for hyperparameter optimization
   # Parameter boundaries are taken from the lightgbm docs and hand-tuned
   # See: https://lightgbm.readthedocs.io/en/latest/Parameters-Tuning.html
@@ -204,7 +218,7 @@ if (cv_enable) {
       lambda_l2           = lightsnip::lambda_l2(lgbm_range$lambda_l2),
       max_bin             = lightsnip::max_bin(lgbm_range$max_bin)
     )
-
+  
   # Use Bayesian tuning to find best performing hyperparameters. This part takes
   # quite a long time, depending on the compute resources available
   lgbm_search <- tune_bayes(
@@ -213,7 +227,7 @@ if (cv_enable) {
     initial = params$cv$initial_set,
     iter = params$cv$max_iterations,
     param_info = lgbm_params,
-    metrics = metric_set(rmse, mape, mae),
+    metrics = metric_set(rmse, mae, mape),
     control = control_bayes(
       verbose = TRUE,
       uncertain = params$cv$no_improve - 2,
@@ -221,13 +235,13 @@ if (cv_enable) {
       seed = params$model$seed
     )
   )
-
+  
   # Save tuning results to file. This is a data frame where each row is one
   # CV iteration
   lgbm_search %>%
     lightsnip::axe_tune_data() %>%
     arrow::write_parquet(paths$output$parameter_raw$local)
-
+  
   # Save the parameter ranges searched while tuning
   lgbm_params %>%
     mutate(range = purrr::map(object, dials::range_get)) %>%
@@ -237,21 +251,20 @@ if (cv_enable) {
       lower, upper
     ) %>%
     arrow::write_parquet(paths$output$parameter_range$local)
-
+  
   # Choose the best model (whichever model minimizes the chosen objective,
   # averaged across CV folds)
   lgbm_final_params <- tibble(
-      engine = params$model$engine,
-      seed = params$model$seed,
-      objective = params$model$objective
-    ) %>%
+    engine = params$model$engine,
+    seed = params$model$seed,
+    objective = params$model$objective
+  ) %>%
     bind_cols(as_tibble(params$model$parameter)) %>%
     bind_cols(select_best(lgbm_search, metric = params$cv$best_metric)) %>%
     select(configuration = .config, everything()) %>%
     arrow::write_parquet(paths$output$parameter_final$local)
-  
 } else {
-
+  
   # If CV is disabled, just use the default set of parameters specified in
   # params.yaml, keeping only the ones used in the model specification
   lgbm_missing_params <- names(params$model$hyperparameter$default)
@@ -259,11 +272,11 @@ if (cv_enable) {
     !lgbm_missing_params %in% parameters(lgbm_wflow)$name
   ]
   lgbm_final_params <- tibble(
-      configuration = "Default",
-      engine = params$model$engine,
-      seed = params$model$seed,
-      objective = params$model$objective
-    ) %>%
+    configuration = "Default",
+    engine = params$model$engine,
+    seed = params$model$seed,
+    objective = params$model$objective
+  ) %>%
     bind_cols(as_tibble(params$model$parameter)) %>%
     bind_cols(as_tibble(params$model$hyperparameter$default)) %>%
     select(-all_of(lgbm_missing_params)) %>%
@@ -305,10 +318,31 @@ lgbm_wflow_final_full_fit <- lgbm_wflow %>%
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # Get predictions on the test set using the training data model. These
-# predictions are used to evaluate model performance on the unseen test set
+# predictions are used to evaluate model performance on the unseen test set.
+# Keep only the variables necessary for evaluation
 test %>%
   mutate(pred_card_initial_fmv = predict(lgbm_wflow_final_fit, test)$.pred) %>%
-  write_parquet(paths$intermediate$test$local)
+  select(
+    meta_year, meta_pin, meta_class, meta_card_num,
+    meta_triad_code, meta_township_code, meta_nbhd_code,
+    loc_cook_municipality_name, loc_chicago_ward_num, loc_census_puma_geoid,
+    loc_census_tract_geoid, loc_school_elementary_district_geoid,
+    loc_school_secondary_district_geoid, loc_school_unified_district_geoid,
+    char_bldg_sf,
+    all_of(c(
+      "prior_far_tot" = params$ratio_study$far_column,
+      "prior_near_tot" = params$ratio_study$near_column
+    )),
+    pred_card_initial_fmv,
+    meta_sale_price, meta_sale_date, meta_sale_document_num
+  ) %>%
+  # Prior year values are AV, not FMV. Multiply by 10 to get FMV for residential
+  mutate(
+    prior_far_tot = prior_far_tot * 10,
+    prior_near_tot = prior_near_tot * 10
+  ) %>%
+  as_tibble() %>%
+  write_parquet(paths$output$test_card$local)
 
 # Save the finalized model object to file so it can be used elsewhere. Note the
 # lgbm_save() function, which uses lgb.save() rather than saveRDS(), since
