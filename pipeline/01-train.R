e766d1e90ec61307068d0e590caf3d5af987db69
Dan Snow <dan@sno.ws>
2023-03-08 15:12:26 +0000

    Update 01-train to reflect res model changes

--- pipeline/01-train.R
+++ pipeline/01-train.R
@@ -2,26 +2,25 @@
 # 1. Setup ---------------------------------------------------------------------
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-# Start the stage timer
+# Start the stage timer and clear logs from prior stage
 tictoc::tic.clearlog()
 tictoc::tic("Train")
 
 # Load libraries and scripts
 options(tidymodels.dark = TRUE)
-library(arrow)
-library(assessr)
-library(butcher)
-library(ccao)
-library(dplyr)
-library(here)
-library(lightgbm)
-library(lightsnip)
-library(lubridate)
-library(stringr)
-library(tictoc)
-library(tidymodels)
-library(vctrs)
-library(yaml)
+suppressPackageStartupMessages({
+  library(arrow)
+  library(butcher)
+  library(ccao)
+  library(dplyr)
+  library(here)
+  library(lightgbm)
+  library(lightsnip)
+  library(tictoc)
+  library(tidymodels)
+  library(vctrs)
+  library(yaml)
+})
 
 # Load helpers and recipes from files
 walk(list.files("R/", "\\.R$", full.names = TRUE), source)
@@ -52,10 +51,12 @@ set.seed(params$model$seed)
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # 2. Prepare Data --------------------------------------------------------------
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+message("Preparing model training data")
 
 # Load the full set of training data, then arrange by sale date in order to
 # facilitate out-of-time sampling/validation
 training_data_full <- read_parquet(paths$input$training$local) %>%
+  filter(!sv_is_outlier) %>%
   arrange(meta_sale_date)
 
 # Create train/test split by time, with most recent observations in the test set
@@ -85,6 +86,7 @@ train_recipe <- model_main_recipe(
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # 3. LightGBM Model ------------------------------------------------------------
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+message("Initializing LightGBM model")
 
 # This is the main model used to value residential condominiums. It uses
 # lightgbm as a backend, which is a boosted tree model similar to xgboost or
@@ -98,50 +100,56 @@ train_recipe <- model_main_recipe(
 # lightgbm as "engine arguments" i.e. things specific to lightgbm, as opposed to
 # model arguments, which are provided by parsnip's boost_tree()
 lgbm_model <- parsnip::boost_tree(
-  trees = params$model$parameter$num_iterations,
-  stop_iter = params$model$parameter$stop_iter
+  stop_iter = params$model$parameter$stop_iter,
+  trees = params$model$parameter$num_iterations
 ) %>%
   set_mode("regression") %>%
   set_engine(
     engine = params$model$engine,
+
+    # Parameters required to make the model deterministic i.e. output the same
+    # predictions if the same hyperparameters are used
     seed = params$model$seed,
-    
-    
+    deterministic = params$model$deterministic,
+    force_row_wise = params$model$force_row_wise,
+
+
     ### 3.1.1. Manual Parameters -----------------------------------------------
-    
+
     # These are static lightgbm-specific engine parameters passed to lgb.train()
     # See lightsnip::train_lightgbm for details
     num_threads = num_threads,
     verbose = params$model$verbose,
-    
+
     # Set the objective function. This is what lightgbm will try to minimize
     objective = params$model$objective,
-    
+
     # Typically set manually along with the number of iterations (trees)
     learning_rate = params$model$parameter$learning_rate,
-    
-    # Names of integer-encoded categorical columns. This is CRITICAL else
+
+    # Names of integer-encoded categorical columns. This is CRITICAL or else
     # lightgbm will treat these columns as numeric
     categorical_feature = params$model$predictor$categorical,
-    
+
     # Enable early stopping using a proportion of each training sample as a
-    # validation set. If lgb.train goes stop_iter() rounds without improvement
+    # validation set. If lgb.train goes `stop_iter` rounds without improvement
     # in the chosen metric, then it will end training early. This saves an
-    # immense amount of time during CV
+    # immense amount of time during CV. WARNING: See issue #82 for more info
     validation = params$model$parameter$validation_prop,
+    sample_type = params$model$parameter$validation_type,
     metric = params$model$parameter$validation_metric,
-    
+
     # Lightsnip custom parameter. Links the value of max_depth to num_leaves
     # using floor(log2(num_leaves)) + add_to_linked_depth. Useful since
     # otherwise Bayesian opt spends time exploring irrelevant parameter space
     link_max_depth = params$model$parameter$link_max_depth,
-    
+
     # Max number of bins that feature values will be bucketed in
     max_bin = params$model$parameter$max_bin,
-    
-    
+
+
     ### 3.1.2. Tuned Parameters ------------------------------------------------
-    
+
     # These are parameters that are tuned using cross-validation. These are the
     # main parameters determining model complexity
     num_leaves = tune(),
@@ -149,13 +157,13 @@ lgbm_model <- parsnip::boost_tree(
     feature_fraction = tune(),
     min_gain_to_split = tune(),
     min_data_in_leaf = tune(),
-    
+
     # Categorical-specific parameters
     max_cat_threshold = tune(),
     min_data_per_group = tune(),
     cat_smooth = tune(),
     cat_l2 = tune(),
-    
+
     # Regularization parameters
     lambda_l1 = tune(),
     lambda_l2 = tune()
@@ -173,40 +181,27 @@ lgbm_wflow <- workflow() %>%
 
 ## 3.2. Cross-Validation -------------------------------------------------------
 
-# Begin CV tuning if enabled. We use Bayesian tuning, as due to the high number
-# of hyperparameters, grid search or random search take a very long time to
-# produce similarly accurate results
+# Begin CV tuning if enabled. We use Bayesian tuning as grid search or random
+# search take a very long time to produce good results due to the high number
+# of hyperparameters
 if (cv_enable) {
+  message("Starting cross-validation")
   
-  # Collapse the first and last CV window into there respective neighbors. This
-  # is done because the first and last period tend to be very small (and
-  # therefore potentially unrepresentative of the larger data set)
-  train <- train %>%
-    mutate(
-      time_split = case_when(
-        time_split == max(time_split) ~ max(time_split) - 1,
-        time_split == min(time_split) ~ min(time_split) + 1,
-        TRUE ~ time_split
-      )
-    )
-  
-  # Using rolling forecast origin resampling to create a cumulative, sliding
-  # window-based training set, where the validation set is always just after the
-  # training set in time. See https://www.tmwr.org/resampling.html#rolling
-  train_folds <- rolling_origin(
-    data = nest(train, data = -time_split),
-    initial = 1, assess = 1, skip = 0, cumulative = TRUE
-  ) %>%
-    mutate(splits = map(splits, ~ make_splits(
-      bind_rows(analysis(.x)$data),
-      bind_rows(assessment(.x)$data)
-    ))) %>%
-    rsample::new_rset(
-      splits = .$splits,
-      ids = .$id,
-      subclass = c("rolling_origin", "rset")
-    )
-  
+  # Using rolling origin resampling to create a cumulative, sliding time window
+  # training set, where the validation set is always the X% of sales following
+  # the training set in time. See https://www.tmwr.org/resampling.html#rolling
+
+  # CRITICAL NOTE: In the folds created here, the validation set is the last X%
+  # of the training set, meaning they overlap! This is because Lightsnip cuts
+  # out the last X% of each training set to use as a validation set for early
+  # stopping. See issue #82 for more information
+  train_folds <- rolling_origin_pct_split(
+    data = train,
+    order_col = meta_sale_date,
+    split_col = time_split,
+    assessment_pct = params$model$parameter$validation_prop
+  )
+
   # Create the parameter search space for hyperparameter optimization
   # Parameter boundaries are taken from the lightgbm docs and hand-tuned
   # See: https://lightgbm.readthedocs.io/en/latest/Parameters-Tuning.html
@@ -227,30 +222,31 @@ if (cv_enable) {
       lambda_l2           = lightsnip::lambda_l2(lgbm_range$lambda_l2),
       neighbors           = dials::neighbors(lgbm_range$neighbors)
     )
-  
+
   # Use Bayesian tuning to find best performing hyperparameters. This part takes
   # quite a long time, depending on the compute resources available
   lgbm_search <- tune_bayes(
     object = lgbm_wflow,
     resamples = train_folds,
-    initial = params$cv$initial_set,
-    iter = params$cv$max_iterations,
+    initial = 2,
+    iter = 1,
     param_info = lgbm_params,
-    metrics = metric_set(rmse, mae, mape),
+    metrics = metric_set(rmse, mape, mae),
     control = control_bayes(
       verbose = TRUE,
       uncertain = params$cv$no_improve - 2,
       no_improve = params$cv$no_improve,
+      extract = extract_num_iterations,
       seed = params$model$seed
     )
   )
-  
-  # Save tuning results to file. This is a data frame where each row is one
+
+  # Save tuning results to file. This is a data.frame where each row is one
   # CV iteration
   lgbm_search %>%
     lightsnip::axe_tune_data() %>%
     arrow::write_parquet(paths$output$parameter_raw$local)
-  
+
   # Save the parameter ranges searched while tuning
   lgbm_params %>%
     mutate(range = purrr::map(object, dials::range_get)) %>%
@@ -260,7 +256,7 @@ if (cv_enable) {
       lower, upper
     ) %>%
     arrow::write_parquet(paths$output$parameter_range$local)
-  
+
   # Choose the best model (whichever model minimizes the chosen objective,
   # averaged across CV folds)
   lgbm_final_params <- tibble(
@@ -268,12 +264,12 @@ if (cv_enable) {
     seed = params$model$seed,
     objective = params$model$objective
   ) %>%
-    bind_cols(as_tibble(params$model$parameter)) %>%
+    bind_cols(select_max_iterations(lgbm_search, metric = params$cv$best_metric)) %>%
+    bind_cols(as_tibble(params$model$parameter) %>% select(-num_iterations)) %>%
     bind_cols(select_best(lgbm_search, metric = params$cv$best_metric)) %>%
     select(configuration = .config, everything()) %>%
     arrow::write_parquet(paths$output$parameter_final$local)
 } else {
-  
   # If CV is disabled, just use the default set of parameters specified in
   # params.yaml, keeping only the ones used in the model specification
   lgbm_missing_params <- names(params$model$hyperparameter$default)
@@ -290,9 +286,9 @@ if (cv_enable) {
     bind_cols(as_tibble(params$model$hyperparameter$default)) %>%
     select(-all_of(lgbm_missing_params)) %>%
     arrow::write_parquet(paths$output$parameter_final$local)
-  
+
   # If CV is disabled, we still need to write empty stub files for any outputs
-  # created by CV. This is so DVC has something to hash/look for
+  # created by CV so DVC has something to hash/look for
   arrow::write_parquet(data.frame(), paths$output$parameter_raw$local)
   arrow::write_parquet(data.frame(), paths$output$parameter_range$local)
 }
@@ -300,22 +296,29 @@ if (cv_enable) {
 
 ## 3.3. Fit Models -------------------------------------------------------------
 
-# NOTE: The model specifications here use early stopping by measuring the change
-# in the objective function on the TRAINING set (rather than the 10% sample
-# validation set used during CV). In practice, this means early stopping is
-# disabled, since you can almost always improve on the training set
+# Finalize the model specification by disabling early stopping, instead using
+# the maximum number of iterations used during the best cross-validation round
+# OR the default `num_iterations` if CV was not performed
+lgbm_model_final <- lgbm_model %>%
+  set_args(
+    stop_iter = NULL,
+    validation = 0,
+    trees = lgbm_final_params$num_iterations
+  )
 
 # Fit the final model using the training data and our final hyperparameters
 # This model is used to measure performance on the test set
+message("Fitting final model on training data")
 lgbm_wflow_final_fit <- lgbm_wflow %>%
-  update_model(lgbm_model %>% set_args(validation = 0)) %>%
+  update_model(lgbm_model_final) %>%
   finalize_workflow(lgbm_final_params) %>%
   fit(data = train)
 
 # Fit the final model using the full data (including the test set) and our final
 # hyperparameters. This model is used for actually assessing all properties
+message("Fitting final model on full data")
 lgbm_wflow_final_full_fit <- lgbm_wflow %>%
-  update_model(lgbm_model %>% set_args(validation = 0)) %>%
+  update_model(lgbm_model_final) %>%
   finalize_workflow(lgbm_final_params) %>%
   fit(data = training_data_full)
 
@@ -325,6 +328,7 @@ lgbm_wflow_final_full_fit <- lgbm_wflow %>%
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # 4. Finalize Models -----------------------------------------------------------
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+message("Finalizing and saving trained model")
 
 # Get predictions on the test set using the training data model. These
 # predictions are used to evaluate model performance on the unseen test set.
@@ -334,10 +338,10 @@ test %>%
   select(
     meta_year, meta_pin, meta_pin10, meta_class, meta_card_num,
     meta_triad_code, meta_township_code, meta_nbhd_code,
-    loc_cook_municipality_name, loc_chicago_ward_num, loc_census_puma_geoid,
+    loc_cook_municipality_name, loc_ward_num, loc_census_puma_geoid,
     loc_census_tract_geoid, loc_school_elementary_district_geoid,
     loc_school_secondary_district_geoid, loc_school_unified_district_geoid,
-    char_unit_sf,
+    char_unit_sf, char_building_sf,
     all_of(c(
       "prior_far_tot" = params$ratio_study$far_column,
       "prior_near_tot" = params$ratio_study$near_column
@@ -375,3 +379,4 @@ bind_rows(tictoc::tic.log(format = FALSE)) %>%
     paths$intermediate$timing$local,
     "model_timing_train.parquet"
   )))
+
