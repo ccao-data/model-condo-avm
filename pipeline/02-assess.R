fb520c651bf2684c1fe61de4f0965cca3b35520c
Damonamajor <damon.major@cookcountyil.gov>
2024-11-13 19:18:42 +0000

    remove code from pipeline

--- pipeline/02-assess.R
+++ pipeline/02-assess.R
@@ -54,48 +54,6 @@ assessment_data_pred <- read_parquet(paths$input$assessment$local) %>%
     )$.pred
   )
 
-assessment_data_pred <- read_parquet(paths$input$assessment$local) %>%
-  as_tibble() %>%
-  mutate(test1 = meta_strata_1,
-         test2 = meta_strata_2)
-
-baked_predictors <- bake(
-  lgbm_final_full_recipe,
-  new_data = assessment_data_pred,
-  everything()
-)
-
-assessment_data_pred <- assessment_data_pred %>%
-  select(-c("meta_strata_1", "meta_strata_2"))
-
-test_join <- baked_predictors %>%
-  inner_join(assessment_data_pred, by = "meta_pin") %>%
-  select(meta_pin, meta_strata_1, meta_strata_2, test1, test2)
-
-
-
-
-baked_predictors <- bake(
-  lgbm_final_full_recipe,
-  new_data = assessment_data_pred,
-  everything()
-)
-
-table(assessment_data_pred$test1, assessment_data_pred$meta_strata_1)
-
-assessment_data_pred <- assessment_data_pred %>%
-  mutate(
-    pred_card_initial_fmv = predict(
-      lgbm_final_full_fit,
-      new_data = baked_predictors
-    )$.pred
-  )
-
-final_data_with_predictors <- assessment_data_pred %>%
-  bind_cols(baked_predictors)
-
-
-
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # 3. Post-Modeling Adjustments -------------------------------------------------
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
