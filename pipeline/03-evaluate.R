9525a2460a343a0277b78a0f1cd3a20c6d8e3ba4
Jean Cochrane <jeancochrane@users.noreply.github.com>
2026-08-12 13:11:20 -0500

    Upgrade R and renv dependencies (#174)
    * Update R and renv dependencies
    
    * Set `pipe_consistency_linter` to `auto` in `.lintr`
    
    * Resolve pre-commmit errors
    
    * Resolve pipeline warnings
    
    * Resolve more pre-commit errors related to line spacing
    
    * Remove stray comment in Dockerfile
    
    * Align `zip` versions between dev and default renv profiles

--- pipeline/03-evaluate.R
+++ pipeline/03-evaluate.R
@@ -37,8 +37,6 @@ run_triad_code <- ccao::town_dict %>%
   pull(triad_code)
 
 
-
-
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # 2. Load Data -----------------------------------------------------------------
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -91,8 +89,6 @@ add_triad_code <- \(data) {
   }
   return(data)
 }
-
-
 
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -168,7 +164,6 @@ gen_agg_stats <- function(data, truth, estimate, bldg_sqft,
     # Aggregate including class
     group_by({{ triad }}, {{ geography }}, {{ class }}) %>%
     summarize(
-
       # Basic summary stats, counts, proportions, etc
       num_pin = n(),
       num_sale = sum(!is.na({{ truth }})),
@@ -329,8 +324,6 @@ gen_agg_stats_quantile <- function(data, truth, estimate,
     )) %>%
     add_triad_code()
 }
-
-
 
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
